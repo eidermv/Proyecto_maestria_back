3d8c26976ddc8c079e5cd1dfb7f1562842a81cdb
Eider Arley Muñoz <eider205v@gmail.com>
2021-05-27 16:58:11 -0500

    Ajuste de ejecutable war y jar

--- src/main/java/co/edu/unicauca/gestordocumental/GestordocumentaApplication.java
+++ src/main/java/co/edu/unicauca/gestordocumental/GestordocumentaApplication.java
@@ -1,13 +1,20 @@
 package co.edu.unicauca.gestordocumental;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
-public class GestordocumentaApplication {
+public class GestordocumentaApplication extends SpringBootServletInitializer {
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(GestordocumentaApplication.class);
+	}
 	@Bean
 	public BCryptPasswordEncoder bCryptPasswordEncoder() {
 		return new BCryptPasswordEncoder();
